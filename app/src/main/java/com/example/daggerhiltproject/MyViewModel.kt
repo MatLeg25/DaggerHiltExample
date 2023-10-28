3a25521e2524998f8f4989d9bd09955b35d12ab9
MatLeg <mat.legierskii@gmail.com>
2023-10-28 20:07:01 +0200

    example of Lazy DI added

--- app/src/main/java/com/example/daggerhiltproject/MyViewModel.kt
+++ app/src/main/java/com/example/daggerhiltproject/MyViewModel.kt
@@ -2,11 +2,23 @@ package com.example.daggerhiltproject
 
 import androidx.lifecycle.ViewModel
 import com.example.daggerhiltproject.domain.repository.MyRepository
+import dagger.Lazy
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
+
+//normally dependency is constructed as soon as we injected,
+// however with the Lazy, is not directly created when is injected
+// but is created when is used
+
 @HiltViewModel
 class MyViewModel @Inject constructor(
-    private val repository: MyRepository
+//    private val repository: MyRepository
+    private val repository: Lazy<MyRepository>
 ): ViewModel() {
+
+    init {
+        repository.get()
+    }
+
 }
